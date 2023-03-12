a7ffc54376896f4abc271965605e3d836d7ff194
Ifeoluwa Sanni <47613365+pheobeayo@users.noreply.github.com>
2023-03-12 21:17:48 +0100

    integrate with smart contract

---
+++ src/connection/metamask.js
@@ -0,0 +1,33 @@
+import { ethers } from "ethers";
+
+export const isMetaMaskInstalled = () => {
+  //Have to check the ethereum binding on the window object to see if it's installed
+  const { ethereum } = window;
+  return Boolean(ethereum && ethereum.isMetaMask);
+};
+
+export const getProvider = () => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  return provider;
+};
+
+export const getSigner = () => {
+  return getProvider().getSigner();
+};
+
+export const connect = () => {
+  return window.ethereum
+    .request({ method: "eth_requestAccounts" })
+    .then((accounts) => {
+      return accounts;
+    })
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log("Please connect to MetaMask.");
+      } else {
+        console.error(err);
+      }
+    });
+};
